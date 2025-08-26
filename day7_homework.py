8f371dd57d59a60e91fc9c5ec58674736cdac84e
secret3694 <1658891320@qq.com>
2025-08-26 11:32:53 +0800

    Add files via upload

--- day7_homework.py
+++ day7_homework.py
@@ -1,3 +1,4 @@
+#每一个batch内找最长padding
 import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer
@@ -9,38 +10,25 @@ def add_eos_to_examples(example):
     example['input_text'] = '%s .' % string
     example['target_text'] = '%s ' % example['target']
     return example
-def convert_to_features(example_batch):
-    input_encodings = tokenizer(example_batch['input_text'], padding="max_length", max_length=16, truncation=True,
-                                return_tensors="pt")#提示词转化为token,保留句子，掩码和token_id
-    target_encodings = tokenizer(example_batch['target_text'], padding="max_length", max_length=16, truncation=True,
-                                 return_tensors="pt").input_ids#句子转化为token，只保留句子，不保留掩码和token_id
-    labels_with_ignore_index = []
-    for labels_example in target_encodings:#一个batch的句子的token,labels_example表示一个句子的token
-        labels_example = [label if label != 0 else -100 for label in labels_example]#token=0就是-100,不然不变
-        labels_with_ignore_index.append(labels_example)#二维列表，每一个元素是一个token的列表
-    encodings = {
-        'input_ids': input_encodings['input_ids'],#提示词的token
-        'attention_mask': input_encodings['attention_mask'],#提示词的掩码
-        'labels': labels_with_ignore_index#句子的token
-    }
-    return encodings
 text_data = text_data.map(add_eos_to_examples, batched=False, remove_columns=text_data.column_names)
-print(text_data[0])
-text_data = text_data.map(convert_to_features, batched=True, remove_columns=text_data.column_names)
-print(text_data[0])
-def custom_collate_fn(batch):
-    input_ids = []
-    attention_mask = []
-    labels = []
-    for item in batch:
-        input_ids += [item["input_ids"]]
-        attention_mask += [item["attention_mask"]]
-        labels += [item["labels"]]
-    return {"input_ids":input_ids,"attention_mask":attention_mask,"labels":labels}
-text_loader = torch.utils.data.DataLoader(text_data,batch_size=10,shuffle=True,num_workers=0,collate_fn=custom_collate_fn)
+def custom_collate_fn(example_batch):
+    input_texts = [example["input_text"] for example in example_batch]
+    target_texts = [example["target_text"] for example in example_batch]
+    input_encodings = tokenizer(input_texts, padding=True, truncation=True,return_tensors="pt")#提示词转化为token,保留句子，掩码和token_id#这个时候对input_texts和target_texts按batch最长编码
+    target_encodings = tokenizer(target_texts, padding=True,truncation=True,return_tensors="pt").input_ids#句子转化为token，只保留句子，不保留掩码和token_id#maxlength默认是模型的最大长度，True是batch的最大长度
+    labels = target_encodings.clone()
+    labels[labels == tokenizer.pad_token_id] = -100
+    return {"input_ids": input_encodings['input_ids'],"attention_mask":input_encodings['attention_mask'],"labels":labels}
+text_loader = torch.utils.data.DataLoader(text_data,batch_size=2,shuffle=False,num_workers=0,collate_fn=custom_collate_fn)
 try:
     for batch in text_loader:
         print(batch)
+        print(tokenizer.decode(batch['input_ids'][0]))
+        print(tokenizer.decode(batch["labels"][0]))
         break
 except Exception as e:
     print("error:", e)
+# {'input_ids': tensor([[49406,  3428,   267,  3965,   267, 42585,   269, 49407],[49406,  3428,   267,  3965,   267, 42585,   269, 49407]]),
+# 'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1],[1, 1, 1, 1, 1, 1, 1, 1]]),
+# 'labels': tensor([[49406, 42585, 32843,  1136,   518,  3965,  -100,  -100,  -100,  -100],[49406,   320, 42585,   533, 14400,  1136,   320,  3965,   269,  -100]])}
+# <|startoftext|>ski , mountain , skier . <|endoftext|>
